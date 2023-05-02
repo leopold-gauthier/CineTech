29f94c9becf128cd92c5d49c56ce81d2c6ecceff
leopold-gauthier <leopold.gauthier-de-porry@laplateforme.io>
2023-05-02 13:31:15 +0200

    add the comment system in the detail page

--- detail.php
+++ detail.php
@@ -1,3 +1,4 @@
+<?php ob_start(); ?>
 <!DOCTYPE html>
 
 
@@ -37,6 +38,139 @@
                 </div>
             </div>
             <div id="commentaire">
+                <div id="add">
+                    <?php
+                    ////////////////////////////////////////// COMMENTAIRE ///////////////////////////////////////////////////
+
+                    // Affichage
+                    if (isset($_SESSION['user']) != null) { ?>
+                        <form method="post" action="">
+                            <div class="mb-3">
+                                <label class="form-label">Commenter</label><br>
+                                <textarea rows="3" class="form-control" name="commentaire"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <input class="btn btn-secondary mb-3" type="submit" name="submit" value="Envoyer le message">
+                            </div>
+                        </form>
+                    <?php
+                    } else { ?>
+                        <a class="bouton" href="./index.php?connect">Se Connecter</a>
+                    <?php
+                    }
+                    ?>
+                </div>
+
+
+
+
+
+                <?php
+                // DECLARATION SQL
+                // commentaire
+                $recupCommentaire = $bdd->prepare("SELECT type, id_film ,`commentaires`.`id`,`commentaires`.`id_utilisateur`, login, avatar, commentaire , date FROM commentaires  JOIN users ON users.id = commentaires.id_utilisateur WHERE id_film = ? ORDER BY date DESC");
+                $recupCommentaire->execute([$_GET['id']]);
+                $commentaire = $recupCommentaire->fetchAll(PDO::FETCH_ASSOC);
+                // reponse
+                $recupReponse = $bdd->query("SELECT users.login, users.avatar, reponses.id, reponses.id_utilisateur,reponses.id_commentaire, reponses.reponse, reponses.date_reponse FROM reponses JOIN users ON reponses.id_utilisateur = users.id JOIN commentaires ON reponses.id_commentaire = commentaires.id ORDER BY date_reponse DESC;");
+                $reponse = $recupReponse->fetchAll(PDO::FETCH_ASSOC);
+
+                ///// AFFICHER COMMENTAIRE ////
+                for ($i = 0; $i < sizeof($commentaire); $i++) :
+                ?>
+                    <div class="message">
+                        <h2><?php if ($commentaire[$i]['login'] === 'admin') { ?>
+                            <?php
+                                echo "<img src='./style/icone-utilisateur-rouge.png'>";
+                            } else {
+                                echo "<img src='./style/icone-utilisateur-vert.png'>";
+                            }
+                            ?>
+                            Posté par <?= $commentaire[$i]['login'] ?> le <?= $commentaire[$i]['date'] ?>
+                        </h2>
+                        <p>
+                            <i>
+                                <?= $commentaire[$i]['commentaire'] ?>
+                            </i>
+                        </p>
+                        <!-- EDITER COMMENTAIRE -->
+                        <div id="modified">
+                            <?php
+                            if (isset($_SESSION['user']->login) == null) {
+                                echo "";
+                            } elseif ($commentaire[$i]['login'] == $_SESSION['user']->login || $_SESSION['user']->login == 'admin') { ?>
+                                <a href="./detail.php?com_id=<?= $commentaire[$i]['id'] ?>">Supprimer</a>
+                            <?php
+                            } elseif ($commentaire[$i]['login'] != $_SESSION['user']->login) {
+                            ?>
+                                <!-- <a class="repondre" href="./detail.php?id=<?php
+                                                                                // $commentaire[$i]['id_film'] 
+                                                                                ?>&type=<?php
+                                                                                        // $commentaire[$i]['type'] 
+                                                                                        ?>>">Répondre</a> -->
+                                <u><a id="monAncre">Répondre</a></u>
+                                <div id="maDiv" style="display:none;">
+                                    <form method="POST">
+                                        <textarea></textarea>
+                                        <br />
+                                        <button type="submit">Répondre</button>
+                                    </form>
+                                </div>
+                            <?php
+                            }
+                            ?>
+                        </div>
+
+                        <!-- ----------------------------------------------------------------------------------------------------------------------------------------------------- -->
+                        <!-- //// AFFICHER REPONSE //// -->
+                        <?php
+                        $recupReponse = $bdd->query("SELECT users.login, users.avatar, reponses.id, reponses.id_utilisateur,reponses.id_commentaire, reponses.reponse, reponses.date_reponse FROM reponses JOIN users ON reponses.id_utilisateur = users.id JOIN commentaires ON reponses.id_commentaire = commentaires.id ORDER BY date_reponse DESC;");
+                        $reponse = $recupReponse->fetchAll();
+
+                        for ($a = 0; $a < sizeof($reponse); $a++) :
+                            if ($reponse[$a]['id_commentaire'] == $livreor[$i]['id']) {
+
+                        ?>
+                                <div class="reponse">
+                                    <h2><?php if ($reponse[$a]['login'] === 'admin') {
+                                            echo "<img height='21px' src='../css/icone-utilisateur-rouge.png'>";
+                                        } else {
+                                            echo "<img height='21px' src='../css/icone-utilisateur-rouge.png'>";
+                                        }
+                                        ?>
+                                        Réponse de <?= $reponse[$a]['login'] ?> le <?= $reponse[$a]['date_reponse'] ?>
+                                    </h2>
+                                    <p>
+                                        <i>
+                                            <?= $reponse[$a]['reponse'] ?>
+                                        </i>
+                                    </p>
+                                    <div id="modified">
+                                        <?php
+                                        if (isset($_SESSION['login']) == null) {
+                                            echo "";
+                                        } elseif ($reponse[$a]['login'] === $_SESSION['login'] || $_SESSION['login'] === 'admin') { ?>
+                                            <a href="./delete_rep.php?id=<?= $reponse[$a]['id'] ?>">Supprimer</a>
+                                            |
+                                            <a href="./reponse.php?edit=<?= $reponse[$a]['id'] ?>">Editer</a>
+                                        <?php
+                                        } elseif ($reponse[$a]['login'] != $_SESSION['login']) {
+                                            echo "";
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+
+                        <?php
+                            }
+                        endfor; //  include_once("./include/reponse-include.php")
+
+                        ?>
+                    </div>
+                <?php
+                endfor;
+                ?>
+
 
             </div>
         </div>
@@ -132,6 +266,65 @@
     <?php }
     }
     ?>
+
+    <script>
+        // Sélectionne l'élément d'ancre et la div à afficher
+        var monAncre = document.getElementById("monAncre");
+        var maDiv = document.getElementById("maDiv");
+
+        // Ajoute un écouteur d'événements au clic sur l'élément d'ancre
+        monAncre.addEventListener("click", function(event) {
+            // event.preventDefault(); // Empêche le comportement par défaut de l'ancre
+
+            // Affiche la div contenant le formulaire
+            maDiv.style.display = "block";
+        });
+        // Ce code sélectionne l'élément d'ancre à l'aide de son ID et ajoute un écouteur d'événements au clic. Lorsque l'utilisateur clique sur l'ancre, l'événement est déclenché, la div contenant le formulaire est affichée en modifiant sa propriété de style et la méthode preventDefault() est utilisée pour empêcher le comportement par défaut de l'ancre (naviguer vers une autre page).
+    </script>
 </body>
 
 </html>
+
+<?php
+
+// BACKEND
+// --- POSTER UN COMMMENTAIRE ---
+if (isset($_POST['commentaire'])) {
+    $id_utilisateur = $_SESSION['user']->id;
+    $commentaire = $_POST['commentaire'];
+    $date = date("Y-m-d H:i:s");
+    if (!empty($commentaire)) {
+        $getUser = $bdd->prepare("INSERT INTO commentaires (commentaire, id_utilisateur, id_film ,type,date) VALUES (?,?,?,?,?)");
+
+        $getUser->bindValue(":id_utilisateur", $id_utilisateur);
+        $getUser->execute([$commentaire, $id_utilisateur, $_GET['id'], $_GET['type'], $date]);
+        $message = "Votre message a bien été posté";
+        header("refresh:1");
+    } else {
+        $update = $bdd->prepare('UPDATE commentaires SET commentaire = ? , date_time_edition = ? WHERE id = ?');
+        $update->execute([$commentaire, $date, $edit_id]);
+        header("refresh:1");
+    }
+} else {
+    $message = "Veuillez écrire un commentaire";
+}
+
+// --- SUPPRIMER COMMENTAIRE ---
+if (isset($_GET['comm_id'])) {
+    if (isset($_GET['comm_id']) == $commentaire[0]['id'] && isset($_SESSION['id']) == $commentaire[0]['id_utilisateur']) {
+        if (isset($_GET['com_id']) and !empty($_GET['com_id'])) {
+
+            $suppr_id = htmlspecialchars($_GET['com_id']);
+
+            $suppr = $bdd->prepare('DELETE FROM commentaires WHERE id = ?');
+            $suppr->execute(array($suppr_id));
+
+            header("refresh:1");
+        } else {
+            // header('refresh:1');
+        }
+    } else {
+        // header('refresh:1');
+    }
+}
+?>
